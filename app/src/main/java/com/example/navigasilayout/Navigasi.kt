6fc568a15d27b8070f5761b1ce49654bd08f3d15
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-06 12:09:17 +0700

    membuat funtion DateApp

---
+++ app/src/main/java/com/example/navigasilayout/Navigasi.kt
@@ -0,0 +1,21 @@
+package com.example.navigasilayout
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DateApp(
+    navController: NavHostController = rememberNavController(),
+    modifier : Modifier
+){
+
+
+    }
+}
